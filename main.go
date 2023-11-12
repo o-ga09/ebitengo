7bf0b994ff555ee6c6e14092f3c8798cd4623026
taiti09 <taitiabe1997@gmail.com>
2023-11-13 00:08:13 +0900

    first game

--- main.go
+++ main.go
@@ -1,82 +1,93 @@
-// Copyright 2018 The Ebiten Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"image"
 	_ "image/png"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
-)
-
-const (
-	screenWidth  = 240
-	screenHeight = 240
-)
-
-const (
-	tileSize = 16
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 var (
-	tilesImage *ebiten.Image
+	runnerImage *ebiten.Image
+	posX        int = 0
+	posY        int = 0
+	runes       []rune
 )
 
-func init() {
-	// Decode an image from the image file's byte slice.
-	img, _, err := image.Decode(bytes.NewReader(images.Tiles_png))
-	if err != nil {
-		log.Fatal(err)
-	}
-	tilesImage = ebiten.NewImageFromImage(img)
-}
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
 
 type Game struct {
-	layers [][]int
+	count int
+}
+
+func NewGame() *Game {
+	return &Game{count: 0}
 }
 
 func (g *Game) Update() error {
+	// 押されていたフレームの間trueになる
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		// キーボードのAが押された場合の処理
+		fmt.Println("A is Pushed !")
+	}
+	// 押されたフレームのみtrueになる
+	if inpututil.IsKeyJustPressed(ebiten.KeyA) {
+		// キーボードのAが押された場合の処理
+		fmt.Println("just A is Pushed !")
+	}
+
+	// キーを離したフレームのみtrueになる
+	if inpututil.IsKeyJustReleased(ebiten.KeyA) {
+		// キーボードのAが離された場合の処理
+		fmt.Println("A is released !")
+	}
+
+	// 左クリックした場合trueになる
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		// マウスの左ボタンが押された場合の処理
+		fmt.Println("LeftClicked !")
+	}
+
+	// 右クリックした場合trueになる
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		// マウスの右ボタンが押された場合の処理
+		fmt.Println("RightClicked !")
+	}
+
+	// 押されたフレームのみtrueになる
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		// キーボードのAが押された場合の処理
+		fmt.Println("just Left is Clicled !")
+	}
+
+	// キーを離したフレームのみtrueになる
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight) {
+		// キーボードのAが離された場合の処理
+		fmt.Println("just Right is Clicked !")
+	}
+
+	posX, posY = ebiten.CursorPosition()
+	runes = ebiten.AppendInputChars(runes)
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	w := tilesImage.Bounds().Dx()
-	tileXCount := w / tileSize
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(0.5, 0.5)
+	op.GeoM.Translate(float64(posX), float64(posY))
+	screen.DrawImage(runnerImage, op)
 
-	// Draw each tile with each DrawImage call.
-	// As the source images of all DrawImage calls are always same,
-	// this rendering is done very efficiently.
-	// For more detail, see https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Image.DrawImage
-	const xCount = screenWidth / tileSize
-	for _, l := range g.layers {
-		for i, t := range l {
-			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64((i%xCount)*tileSize), float64((i/xCount)*tileSize))
-
-			sx := (t % tileXCount) * tileSize
-			sy := (t / tileXCount) * tileSize
-			screen.DrawImage(tilesImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image), op)
-		}
-	}
-
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.ActualTPS()))
+	// runesに入力された文字情報があるので下記のように出力することが可能
+	ebitenutil.DebugPrint(screen, string(runes))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("X : %d, Y : %d", posX, posY))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -84,52 +95,30 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	g := &Game{
-		layers: [][]int{
-			{
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 218, 243, 243, 243, 243, 243, 243, 243, 243, 243, 218, 243, 244, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+	// ウィンドウサイズの指定
+	ebiten.SetWindowSize(640, 480)
+	// ウィンドウタイトルの指定
+	ebiten.SetWindowTitle("Hello, World!")
 
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 244, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 219, 243, 243, 243, 219, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
+	// 画像ファイルを開く
+	file, err := os.Open("img/gopher.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
 
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 218, 243, 243, 243, 243, 243, 243, 243, 243, 243, 244, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-			},
-			{
-				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 26, 27, 28, 29, 30, 31, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 51, 52, 53, 54, 55, 56, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 76, 77, 78, 79, 80, 81, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 101, 102, 103, 104, 105, 106, 0, 0, 0, 0,
-
-				0, 0, 0, 0, 0, 126, 127, 128, 129, 130, 131, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 303, 303, 245, 242, 303, 303, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-			},
-		},
+	// 画像を読み込む
+	img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
-	ebiten.SetWindowTitle("Tiles (Ebitengine Demo)")
-	if err := ebiten.RunGame(g); err != nil {
+	runnerImage = ebiten.NewImageFromImage(img)
+
+	game := NewGame()
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
